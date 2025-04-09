0a6fc5528f2ad70d36859a753766ef911eac2e8b
Rajeshnaitham <naithamrajesh428@gmail.com>
2025-04-09 20:51:27 +0530

    changes in api

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,15 @@ import LandingPage from './vendorDashboard/pages/LandingPage'
 import {Routes,Route} from 'react-router-dom'
 
 import "./App.css"
+import NavBar from './vendorDashboard/components/NavBar'
+import Login from './vendorDashboard/components/forms/Login'
 import NotFound from './vendorDashboard/components/NotFound'
 
 const App = () => {
   return (
     <div>
       <Routes>
-        <Route path='/' element={<LandingPage/>}/>
+        <Route path='/apple' element={<LandingPage/>}/>
         <Route path='/*' element={<NotFound/>}/>
       </Routes>
 
